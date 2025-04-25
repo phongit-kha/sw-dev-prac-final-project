825ac630eae0c80117fba36ea773c0875155541b
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2025-04-25 23:16:38 +0700

    feat: add Dentist model

---
+++ models/Dentist.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+
+const DentistSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add name"],
+      unique: true,
+      trim: true,
+      maxlength: [50, "Name can not be more than 50 characters"],
+    },
+    yearofExperience: {
+      type: Number,
+      required: [true, "Please add year of experience"],
+    },
+    gender: {
+      type: String,
+      enum: ["Male", "Female", "LGBTQ+"],
+      required: [true, "Please add your gender"],
+    },
+    address: {
+      type: String,
+      required: [true, "Please add an address"],
+    },
+    district: {
+      type: String,
+      required: [true, "Please add a district"],
+    },
+    province: {
+      type: String,
+      required: [true, "Please add a province"],
+    },
+    postalcode: {
+      type: String,
+      required: [true, "Please add a postalcode"],
+      maxlength: [50, "Postal Code can not be more than 5 digits"],
+    },
+    tel: {
+      type: String,
+    },
+    region: {
+      type: String,
+      required: [true, "Please add a region"],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+//Cascade delete bookings when a hospital is deleted
+DentistSchema.pre(
+  "deleteOne",
+  { document: true, query: false },
+  async function (next) {
+    console.log(`Bookings being removed from dentist ${this._id}`);
+    await this.model("Booking").deleteMany({ dentist: this._id });
+    next();
+  }
+);
+
+//Reverse populate with virtuals
+DentistSchema.virtual("bookings", {
+  ref: "Booking",
+  localField: "_id",
+  foreignField: "dentist",
+  justOne: false,
+});
+
+module.exports = mongoose.model("Dentist", DentistSchema);
